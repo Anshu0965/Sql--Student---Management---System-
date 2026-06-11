8658292d107abc329292b6c43c9da27ccda58ba5
Anshu0965 <anshukri6207@gmail.com>
2026-06-11 10:29:58 +0530

    Add SQL project

--- Student _management.sql
+++ Student _management.sql
@@ -1,1 +1,18 @@
+CREATE TABLE Courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50)
+);
+
+INSERT INTO Students VALUES
+(1,'Anshu',22,'Delhi'),
+(2,'Rahul',21,'Noida'),
+(3,'Priya',23,'Mumbai');
+
+INSERT INTO Courses VALUES
+(101,'SQL'),
+(102,'Python'),
+(103,'Power BI');
+
+SELECT * FROM Students;
+SELECT * FROM Courses;
 
