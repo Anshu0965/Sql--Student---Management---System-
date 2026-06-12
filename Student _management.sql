9fa0cd1dea55c6e2ceaa05b01f4d817fe9b28047
Anshu0965 <anshukri6207@gmail.com>
2026-06-12 11:36:32 +0530

    Add enrollment table and SQL queries

--- Student _management.sql
+++ Student _management.sql
@@ -16,3 +16,31 @@ INSERT INTO Courses VALUES
 SELECT * FROM Students;
 SELECT * FROM Courses;
 
+
+
+CREATE TABLE Enrollments (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    enrollment_date DATE,
+    FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
+
+INSERT INTO Enrollments VALUES
+(1,1,101,'2025-06-01'),
+(2,2,102,'2025-06-02'),
+(3,3,103,'2025-06-03');
+
+SELECT *
+FROM Students
+ORDER BY age DESC;
+
+SELECT
+s.student_name,
+c.course_name
+FROM Students s
+JOIN Enrollments e
+ON s.student_id = e.student_id
+JOIN Courses c
+ON e.course_id = c.course_id;
